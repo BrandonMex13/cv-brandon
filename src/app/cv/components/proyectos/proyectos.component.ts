136aee1b5bcbd84b91db43029b91c41ebf8b7db6
Brandon <brandonmex13@gmail.com>
2023-09-28 22:13:26 -0700

    FEAT: actualizacion añadiendo nuevos proyectos

--- src/app/cv/components/proyectos/proyectos.component.ts
+++ src/app/cv/components/proyectos/proyectos.component.ts
@@ -12,17 +12,35 @@ export class ProyectosComponent implements OnInit {
 
     date : Date = new Date();
 
-    @Input('heroe') heroe !: number;
-
     constructor() { }
 
     ngOnInit(): void {
 
         this.proyectosPersonales = [
             {
+                idProyecto       : 6,
+                nombreProyecto   : "Web Freelancer - Presentacion",
+                descripcion      : "Pequeña web de presentación como Freelancer",
+                fechaInicio      : this.date,
+                fechaTerminacion : this.date,
+                lenguaje         : ["HTML", " CSS", " javascript" ],
+                routerLink       : "https://web-freelancer-brandon.netlify.app/",
+                imglink          : "web-freelancer.png"
+            },
+            {
+                idProyecto       : 6,
+                nombreProyecto   : "Web Store",
+                descripcion      : "Simulación de una tienda en linea",
+                fechaInicio      : this.date,
+                fechaTerminacion : this.date,
+                lenguaje         : ["HTML", " CSS", " javascript" ],
+                routerLink       : "https://web-store-brandon.netlify.app/",
+                imglink          : "frontend-store.png"
+            },
+            {
                 idProyecto       : 1,
                 nombreProyecto   : "Gifs App",
-                descripcion      : "Consulta de catalogo de gifs con el uso de una API",
+                descripcion      : "Consulta de catálogo de gifs con el uso de una API",
                 fechaInicio      : this.date,
                 fechaTerminacion : this.date,
                 lenguaje         : ["Angular", " Typescript", " HTML", " CSS" ],
@@ -32,7 +50,7 @@ export class ProyectosComponent implements OnInit {
             {
                 idProyecto       : 2,
                 nombreProyecto   : "Paises",
-                descripcion      : "Consulta de paises con el uso de una API",
+                descripcion      : "Consulta de países con el uso de una API",
                 fechaInicio      : this.date,
                 fechaTerminacion : this.date,
                 lenguaje         : ["Angular", " Typescript", " HTML", " CSS" ],
@@ -42,7 +60,7 @@ export class ProyectosComponent implements OnInit {
             {
                 idProyecto       : 3,
                 nombreProyecto   : "Pipes Angular",
-                descripcion      : "Conocimiento e implementacion de los pipes de angular",
+                descripcion      : "Conocimiento e implementación de los pipes de angular",
                 fechaInicio      : this.date,
                 fechaTerminacion : this.date,
                 lenguaje         : ["Angular", " Typescript", " HTML", " CSS" ],
@@ -50,26 +68,27 @@ export class ProyectosComponent implements OnInit {
                 imglink          : "pipes-app.png"
             },
             {
-                idProyecto       : 4,
-                nombreProyecto   : "Sistema de prestamos",
-                descripcion      : "Sistema de prestamos con tabla de amortizacion",
-                fechaInicio      : this.date,
-                fechaTerminacion : this.date,
-                lenguaje         : ["Javascript", " Boostrap", " PHP", " HTML", " CSS" ],
-                routerLink       : "https://github.com/BrandonMex13/sistemas-web-prestamos",
-                imglink          : "sistema-prestamos.png"
-            },
-            {
                 idProyecto       : 5,
                 nombreProyecto   : "Bouncing DVD",
-                descripcion      : "Clasico logo de DVD rebotando en la pantalla",
+                descripcion      : "Clásico logo de DVD rebotando en la pantalla",
                 fechaInicio      : this.date,
                 fechaTerminacion : this.date,
                 lenguaje         : ["HTML", " CSS" ],
                 routerLink       : "https://bouncing-dvd-brandon.netlify.app/",
                 imglink          : "bouncing-dvd-app.png"
+            },
+            {
+                idProyecto       : 4,
+                nombreProyecto   : "Sistema de préstamos",
+                descripcion      : "Sistema de préstamos con tabla de amortización",
+                fechaInicio      : this.date,
+                fechaTerminacion : this.date,
+                lenguaje         : ["Javascript", " Boostrap", " PHP", " HTML", " CSS" ],
+                routerLink       : "https://github.com/BrandonMex13/sistemas-web-prestamos",
+                imglink          : "sistema-prestamos.png"
             }
         ];
+
     }
 
 }
